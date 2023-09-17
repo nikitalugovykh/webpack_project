46efec6ab84c15de992a81a254b97088f54841d9
nikitalugovykh <nikita_ligovykh@mail.ru>
2023-09-17 19:09:29 +0200

    chore: added errorBoundary

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,26 @@
+import { type FC } from 'react'
+import { cn } from 'shared/libs/cn'
+import styles from './PageError.module.scss'
+import { useTranslation } from 'react-i18next'
+import { Button } from 'shared/ui/Button'
+
+interface PageErrorProps {
+    className?: string
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+    const { t } = useTranslation()
+
+    const reloadPage = (): void => {
+        location.reload()
+    }
+
+    return (
+        <div className={cn(styles.PageError, {}, [className])}>
+            <p>
+                {t('Something went wrong')}
+            </p>
+            <Button onClick={reloadPage}>{t('Update page')}</Button>
+        </div>
+    )
+}
